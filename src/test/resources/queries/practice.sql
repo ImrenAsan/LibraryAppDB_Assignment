68a3276f5de567aaa03d0ccddf70ed00605a6092
ImrenAsan <imrenasan@protonmail.com>
2023-11-22 10:08:06 +0100

    updates

--- src/test/resources/queries/practice.sql
+++ src/test/resources/queries/practice.sql
@@ -7,3 +7,16 @@ FROM books B
     select  count(id) from users; -- 4891
 
     select count(distinct id) from users; -- 4891
+
+    select full_name from users;
+
+select name from books
+where name = 'Head First Java';
+
+
+select full_name,b.name,bb.borrowed_date
+from users u
+    inner join book_borrow bb on u.id = bb.user_id
+    inner join books b on bb.book_id = b.id
+where full_name='Test Student 1’ b.name=‘Head First Java’
+order by 3 desc;
